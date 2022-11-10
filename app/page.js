194ea4a32a991fc25e666e9b9c11d10b160a7170
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-11-10 23:14:45 +0500

    build: basic upgrade to next.js 13

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="bg-green-100 text-green-500">I am a home page</h1>
+    </div>
+  );
+};
+
+export default Home;
