d76669dd7dfdf8cc524f4a16517d34a3d3fb8186
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-11-10 23:50:50 +0500

    build: add absolute Imports && module path aliases

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Buttons from "@/components/Buttons";
 
 const Home = () => {
   return (
     <div>
       <h1 className="bg-green-100 text-green-500">I am a home page</h1>
+      <Buttons />
     </div>
   );
 };
